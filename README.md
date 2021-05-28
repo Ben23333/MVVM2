d07134516860302bfb3d9b1d0a6043d5f25123e1
Benone <877467813@qq.com>
2021-05-28 16:19:57 +0800

    完成applicaiton类，工具类完成部分，全局常数类

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MVVM2
+# WanAndroid-MVVM
+玩 Android 客户端，采用 kotlin 语言，Material Design 风格，根据 MVVM 架构使用 Jetpack 架构组件搭建了整套框架
+
+
---
+++ app/src/main/java/com/benone/mvvm2/Constants.kt
@@ -0,0 +1,9 @@
+package com.benone.mvvm2
+
+object Constants {
+
+    const val WEB_TITLE = "web_title"
+    const val WEB_URL = "web_url"
+    const val KNOWLEDGE = "knowledge"
+    const val PERMISSION_CODE = 100
+}
