085992286d0d948af2b901d8bcde8f5ddea7beac
Benone <877467813@qq.com>
2021-05-31 15:06:28 +0800

    补全ToastUtils

--- app/src/main/java/com/benone/mvvm2/utils/ToastUtils.kt
+++ app/src/main/java/com/benone/mvvm2/utils/ToastUtils.kt
@@ -2,6 +2,7 @@ package com.benone.mvvm2.utils
 
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 import com.benone.mvvm2.R
 import com.benone.mvvm2.WanApplication
@@ -15,6 +16,7 @@ class ToastUtils {
         fun show(content:String){
             val inflater = LayoutInflater.from(WanApplication.instance)
             val view = inflater.inflate(R.layout.toast_layout,null)
+            val text= view.findViewById<TextView>(R.id.toast_text)
             text.text = content
             toast = Toast(WanApplication.instance)
             toast!!.setGravity(Gravity.CENTER,0,0)
